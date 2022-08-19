1abd0025540468108de7de2f9439bd05537026f1
ssun30 <sun.su@northeastern.edu>
2022-08-19 13:36:31 -0400

    Fixed typos in loadShrlib

--- longString.m
+++ longString.m
@@ -1,7 +1,8 @@
 function str = longString
+    buflen = calllib('shrlibsample', 'createLongStr', 0, '');
     aa = zeros(1:buflen);
     ptr = libpointer('cstring', aa);
-    [iok, str] = calllib('shrlibsample', 'createLongStr', ptr);
+    [iok, str] = calllib('shrlibsample', 'createLongStr', buflen, ptr);
     if iok ~= 1
         error('This is an error message');
     end
