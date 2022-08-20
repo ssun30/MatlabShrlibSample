e4c887b70390464c745f25547e5a1782d715198d
Richard West <r.west@northeastern.edu>
2022-08-19 22:15:10 -0400

    Added get(ptr) debug statements in the longString code

--- longString.m
+++ longString.m
@@ -2,9 +2,11 @@ function str = longString
     buflen = calllib('shrlibsample', 'createLongStr', 0, '');
     aa = repelem('a',buflen)
     ptr = libpointer('cstring', aa);
+    get(ptr)
     [iok, str] = calllib('shrlibsample', 'createLongStr', buflen, ptr);
     if iok ~= 0
         error('This is an error message');
     end
+    get(ptr)
     warning(['the pointer value is ',  ptr.Value])
 end
