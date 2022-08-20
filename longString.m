39b33ed30567f729928ff2065627760fe684419f
Richard West <r.west@northeastern.edu>
2022-08-19 22:18:54 -0400

    ptr = libpointer('cstring', aa);
    Same behaviour.
    Pointer
           Value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
        DataType: 'cstring'
    
    But ans = 'abccdefghijklmnopqrstuvwxyzabccdefghijklmnopqrstuvwxyzabccdefghijklmnopqrstuvwxyz'

--- longString.m
+++ longString.m
@@ -1,7 +1,7 @@
 function str = longString
     buflen = calllib('shrlibsample', 'createLongStr', 0, '');
     aa = repelem('a',buflen)
-    ptr = libpointer('stringPtr', aa);
+    ptr = libpointer('cstring', aa);
     get(ptr)
     [iok, str] = calllib('shrlibsample', 'createLongStr', buflen, ptr);
     if iok ~= 0
